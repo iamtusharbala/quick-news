9ba0c58e6e7c202396f17b7dd035c2929d40f79c
Tushar Balakrishnan <tusharbala284@gmail.com>
2024-07-11 05:40:48 +0530

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import React from 'react'
 import Navbar from './Components/Navbar'
 import News from './Components/News'
+import Footer from './Components/Footer'
 
 const App = () => {
   return (
     <>
       <Navbar />
       <News />
+      <Footer />
     </>
   )
 }
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='footer text-center'>Made with ❤️ in Kochi. | By <a href="https://github.com/iamtusharbala" target="_blank">Tushar Balakrishnan</a></div>
+    )
+}
+
+export default Footer
